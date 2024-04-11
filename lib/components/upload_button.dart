b3c0d4cc9d86888fb2014c275f12bbd7108775f1
souvik-blade <souvikbladion@gmail.com>
2024-04-11 21:17:04 +0530

    fixed the the parent data widget error from both addmin assignment and user assignment page.  And swapped the add pdf and upload button.

--- lib/components/upload_button.dart
+++ lib/components/upload_button.dart
@@ -3,20 +3,19 @@ import 'package:flutter/material.dart';
 class UploadButton extends StatelessWidget {
   final String text;
   final void Function()? onTap;
-  UploadButton({required this.text, required this.onTap});
+  const UploadButton({super.key, required this.text, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Expanded(
-        child: Container(
-          padding: EdgeInsets.all(25),
-          margin: EdgeInsets.symmetric(horizontal: 25),
-          decoration: BoxDecoration(
-              color: Colors.teal[100], borderRadius: BorderRadius.circular(8)),
-          child: Center(child: Text(text)),
-        ),
+      child: Container(
+        padding: EdgeInsets.all(25),
+        margin: EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 9, 104, 96),
+            borderRadius: BorderRadius.circular(8)),
+        child: Center(child: Text(text)),
       ),
     );
   }
--- lib/pages/assignments_page.dart
+++ lib/pages/assignments_page.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:colabb/components/assignment_tile.dart';
 import 'package:flutter/material.dart';
-import '../components/my_bottomappbar.dart';
 
 class AssignmentPage extends StatelessWidget {
   static const String id = "assignmentid";
